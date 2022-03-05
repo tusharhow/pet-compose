8dcd1d799db3d5d56233be9f21efe749aad26292
tusharhow <tusharsoftdev@gmail.com>
2022-03-04 22:55:33 -0800

    Initial Commit

---
+++ app/src/main/java/com/example/puppyadoptionapp/data/DogData.kt
@@ -0,0 +1,26 @@
+package com.example.puppyadoptionapp.data
+import com.example.puppyadoptionapp.R
+
+data class DogData(
+    val id:Int,
+    val name:String,
+    val sex:String,
+
+    val age: Int,
+    val weight: Double,
+    val imagePath: Int,
+    val desc:String,
+)
+
+
+var DogsList = mutableListOf<DogData>(
+    DogData(0,"Bobby","Male",2,1.3, R.drawable.puppy1,"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"),
+    DogData(1,"Leia","Female",1,1.1, R.drawable.puppy2,"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"),
+    DogData(2,"Rex","Male",4,3.0, R.drawable.puppy3,"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"),
+    DogData(3,"Melo","Male",2,1.1, R.drawable.puppy4,"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"),
+    DogData(4,"Shuya","Male",1,2.8, R.drawable.puppy5,"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"),
+    DogData(5,"Maki","Female",1,1.8, R.drawable.puppy6,"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"),
+    DogData(6,"Kiba","Male",2,1.0, R.drawable.puppy7,"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"),
+    DogData(7,"Kurama","Male",2,1.0, R.drawable.puppy8,"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"),
+
+)
